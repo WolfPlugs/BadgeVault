185ad3403e7b09d4156795c530d8e5b7025cfd30
Aiden <amb@aspy.dev>
2026-06-21 23:52:58 -0400

    actually remove existing blocked users

--- pull.mjs
+++ pull.mjs
@@ -1,14 +1,15 @@
-import { writeFileSync } from 'fs';
+import { rmSync, writeFileSync } from 'fs';
 
 // Fetch JSON array of users
 const data = await fetch(`https://api.obamabot.me/v2/badges/getAllUsers?key=${process.env.SPECIAL_KEY}`).then(res => res.json());
 
 // Loop through the users in the database
 for (const user of data) {
+    const filePath = "./User/" + user.userId + ".json";
     if(user.blocked){
+        rmSync(filePath, {force: true})
         continue;
     }
-    const filePath = "./User/" + user.userId + ".json";
 
     // Remove pending badges & blocked users
     for(let i=0; i<user.badges.length; ++i){
