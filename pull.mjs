12de2e1225fce12463a5965d0e28521c12227859
SomeAspy <amb@aspy.dev>
2026-07-02 15:31:56 -0400

    copying.txt

--- pull.mjs
+++ pull.mjs
@@ -1,26 +1,27 @@
-import { rmSync, writeFileSync } from 'fs';
+import { rmSync, writeFileSync } from "node:fs";
 
 // Fetch JSON array of users
-const data = await fetch(`https://api.obamabot.me/v2/badges/getAllUsers?key=${process.env.SPECIAL_KEY}`).then(res => res.json());
+const data = await fetch(
+	`https://api.obamabot.me/v2/badges/getAllUsers?key=${process.env.SPECIAL_KEY}`,
+).then((res) => res.json());
 
 // Loop through the users in the database
 for (const user of data) {
-    const filePath = "./User/" + user.userId + ".json";
-    if(user.blocked){
-        rmSync(filePath, {force: true})
-        continue;
-    }
+	const filePath = `./User/${user.userId}.json`;
+	if (user.blocked) {
+		rmSync(filePath, { force: true });
+		continue;
+	}
 
-    // Remove pending badges & blocked users
-    for(let i=0; i<user.badges.length; ++i){
+	// Remove pending badges
+	for (let i = 0; i < user.badges.length; ++i) {
+		if (user.badges[i].pending) {
+			user.badges.splice(i, 1);
+		}
+	}
 
-        if(user.badges[i].pending){
-            user.badges.splice(i,1);
-        }
-    }
+	const userData = JSON.stringify(user);
 
-    const userData = JSON.stringify(user);
-    
-    // Write every single user to the folder
-    writeFileSync(filePath, userData);
+	// Write every single user to the folder
+	writeFileSync(filePath, userData);
 }
